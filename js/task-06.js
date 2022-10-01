93f285a4c713d7ea7fc29b30f00651affc9ccef2
B1lkao <hrabousolesia@gmail.com>
2022-10-01 16:31:15 +0300

    Доробила дз

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#validation-input');
+const onInputBlur = event => {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
+    if (event.currentTarget.value.trim().length === Number(input.dataset.length)) 
+        input.classList.replace('invalid','valid');
+}
+input.addEventListener('blur', onInputBlur);
